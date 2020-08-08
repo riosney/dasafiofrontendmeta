701fd3a6f422b0620a5021d1c5300e2bf0480ad6
Riosney Santos <riosney_s@hotmail.com>
2020-08-08 09:53:14 -0300

    desafio meta back-end

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,11 @@
+export class Cliente {
+  id: number;
+  nome: string;
+  sexo: string;
+  email: string;
+  dataNascimento: string;
+  naturalidade: string;
+  nacionalidade: string;
+  cpf: string;
+  dataCadastro: string;
+}
